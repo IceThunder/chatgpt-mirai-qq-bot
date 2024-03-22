1c1fd10618a90492e6e6ec73da50c16fe2d97ca3
IceThunder <tl_kid@hotmail.com>
2024-03-23 02:22:53 +0800

    fix text

--- platforms/feishu_bot.py
+++ platforms/feishu_bot.py
@@ -156,7 +156,7 @@ def _send_text(receive_id, msg):
         .request_body(CreateMessageRequestBody.builder()
                       .receive_id(receive_id)
                       .msg_type("text")
-                      .content({"text": msg})
+                      .content(lark.JSON.marshal({"text": msg}))
                       .build()) \
         .build()
 
